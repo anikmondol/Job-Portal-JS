2fc5980b5ae8c81b104ee284b6250f22e1d1c548
anikmondol <anikmondol558363@gmail.com>
2024-11-03 21:37:51 +0600

    Job-Portal all device  responsive done  desing done

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
 const bar = document.querySelector("#bar");
-const nav = document.querySelector(".nav_list");
+const nav = document.querySelector("#navbar");
 const close = document.querySelector("#close");
-
-
-
-
 
 if (bar) {
     bar.addEventListener("click", e =>{
@@ -17,3 +13,27 @@ if (close) {
         nav.classList.remove("active");
     } )
 }
+
+
+
+ // Get the navbar element
+ const navbar = document.querySelector("#header");
+
+
+ // Get the offset position of the navbar
+ const sticky = navbar.offsetTop + 600;
+
+ // Add the sticky class to the navbar when you reach its scroll position
+ // Remove the sticky class when you leave the scroll position
+ function stickyNavbar() {
+    //  console.log(window.pageYOffset);
+     if (window.pageYOffset >= sticky) {
+         navbar.classList.add("sticky");
+     } else {
+         navbar.classList.remove("sticky");
+     }
+ }
+ // When the user scrolls the page, execute stickyNavbar
+ window.onscroll = function() {
+     stickyNavbar();
+ };
